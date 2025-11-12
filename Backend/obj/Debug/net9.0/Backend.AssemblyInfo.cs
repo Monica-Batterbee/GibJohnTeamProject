05cea95d7fe983683c231df643359bfc07a93a10
Monica-Batterbee <bat24002645@students.wnc.ac.uk>
2025-11-12 12:10:01 +0000

    test

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6e743ebc6dcf96209f002bac41809b1fc22ceda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2422cd8ed8f334631af2ac1dfdb1847db9c1e053")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
